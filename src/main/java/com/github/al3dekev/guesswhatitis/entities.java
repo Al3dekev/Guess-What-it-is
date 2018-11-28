e5b3f8577f760cd139b6587529955942c6bcbcc2
Al3dekev <quentin1998@me.com>
2018-11-28 17:45:41 +0100

    Entities finished

--- src/main/java/com/github/al3dekev/guesswhatitis/entities.java
+++ src/main/java/com/github/al3dekev/guesswhatitis/entities.java
@@ -1,4 +1,28 @@
 package com.github.al3dekev.guesswhatitis;
 
 public class entities {
+
+    String name;
+    int stick;
+
+    public void choosingAStickAlert(){
+        System.out.println(this.getName()+" has choose a stick.");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStick() {
+        return stick;
+    }
+
+    public void setStick(int stick) {
+        this.stick = stick;
+        this.choosingAStickAlert();
+    }
 }
--- src/main/java/com/github/al3dekev/guesswhatitis/master.java
+++ src/main/java/com/github/al3dekev/guesswhatitis/master.java
@@ -1,4 +1,27 @@
 package com.github.al3dekev.guesswhatitis;
 
 public class master extends entities{
+
+
+
+    public void wrongChoiceAlert(){
+        System.out.println(this.getName()+": you did the wrong choice, Billy");
+    }
+
+    public void goodChoiceAlert(){
+        System.out.print(this.getName()+": That was a good choice ! You won Billy !");
+    }
+
+    public void introAlert(){
+        System.out.println(this.getName()+": Welcome to the game, Billy! you'll never know what's my choose number !");
+    }
+
+    public void outroAlert(){
+        System.out.println(this.getName()+": Good job, Billy!");
+    }
+
+
+
+
+
 }
